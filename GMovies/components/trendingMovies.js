ca4ba5b2cf8428141a77c7568b3230f4c05b8df2
kamilisimionato <kamili.simionato@etec.sp.gov.br>
2024-10-21 12:25:05 -0300

    falta botar imagemmm

---
+++ GMovies/components/trendingMovies.js
@@ -0,0 +1,32 @@
+import { View, Text, Dimensions, TouchableWithoutFeedback, Image } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+import Carousel from 'react-native-reanimated-carousel'
+
+var { width, height } = Dimensions.get('window');
+
+export default function TrendingMovies({ data }) {
+    const navigation = useNavigation();
+    const handleClick = (item) => {
+        navigation.navigate('Movie', item);
+    }
+  return (
+    <View className="mb-8">
+      <Text className="text-white text-xl mx-4">Em alta</Text>
+        <Carousel 
+        
+        />
+    </View>
+  )
+}
+
+const MovieCard = ({item, handleClick}) => {
+    return (
+        <TouchableWithoutFeedback onPress={() => handleClick(item)}>
+            <Image 
+            source={require('')}
+            style={{width: '100%', height: '100%'}}
+            className="rounded-2xl"
+            />
+        </TouchableWithoutFeedback>
+    )
+}
